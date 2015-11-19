3bef1f578cec77f6986f68fc4636a216938c7e5b
fabio franci <fabio.franci@gmail.com>
2015-11-19 12:27:54 +0100

    Vai

--- www/js/index.js
+++ www/js/index.js
@@ -1172,10 +1172,10 @@ function onDeviceReady() {
                             //datiRiga+="<a href='#singola_visita?id="+dati.rows.item(i).codice_visita+"'><button data-theme='f'> Visita del "+dati.rows.item(i).data_inizio_visita+"</button></a>";
                             //datiRiga+='<li><a class="singola_visita_link" href="#singola_visita?id='+dati.rows.item(i).id_locale+'">'+dati.rows.item(i).data_inizio_visita+'</a></li>';
                             alert("Ancora da visionare: "+dati.rows.item(i).nome);
-                            //datiRiga+="<a href='#postazione_mancante?id="+dati.rows.item(i).codice_ispezione+"'><button data-theme='f'> "+dati.rows.item(i).nome+"</button></a>";
+                            datiRiga+="<a href='#postazione_mancante?id="+dati.rows.item(i).codice_ispezione+"'><button data-theme='f'> "+dati.rows.item(i).nome+"</button></a>";
                         }
-                        //$("#postazionimancanti_list").html('<h4>Ancora da visionare</h4>');
-                        //$("#postazionimancanti_list").append(datiRiga);
+                        $("#postazionimancanti_list").html('<h4>Ancora da visionare</h4>');
+                        $("#postazionimancanti_list").append(datiRiga);
                         //$("#postazione_mancante").trigger("create");
                     } else {
                         alert("OK! Tutte le postazioni sono state visitate! Compila il modulo del certificato!");
