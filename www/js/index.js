3bffb3ae5ae360da3a2eec8c67a1dfec20978320
fabio franci <fabio.franci@gmail.com>
2015-11-19 18:56:47 +0100

    Vai

--- www/js/index.js
+++ www/js/index.js
@@ -1248,7 +1248,7 @@ function onDeviceReady() {
                             //datiRiga+="<a href='#singola_visita?id="+dati.rows.item(i).codice_visita+"'><button data-theme='f'> Visita del "+dati.rows.item(i).data_inizio_visita+"</button></a>";
                             //datiRiga+='<li><a class="singola_visita_link" href="#singola_visita?id='+dati.rows.item(i).id_locale+'">'+dati.rows.item(i).data_inizio_visita+'</a></li>';
                             //alert("Ancora da visionare: "+dati.rows.item(i).nome);
-                            datiRiga+="<li><a href='#home' class='postazionemancantelink' codiceispezione='postazionemancante_"+dati.rows.item(i).codice_ispezione+"'> "+dati.rows.item(i).nome+"</a></li>";
+                            datiRiga+="<li><a class='postazionemancantelink' codiceispezione='postazionemancante_"+dati.rows.item(i).codice_ispezione+"'> "+dati.rows.item(i).nome+"</a></li>";
                         }
                         $("#listapostazionimancanti").html(datiRiga);
                         $('#listapostazionimancanti').listview().listview('refresh');
