637cae4e9f027c61221fc8b064d37f61a4b38cf2
fabio franci <fabio.franci@gmail.com>
2015-11-19 17:42:33 +0100

    Vai

--- www/js/index.js
+++ www/js/index.js
@@ -1248,10 +1248,13 @@ function onDeviceReady() {
                             //datiRiga+="<a href='#singola_visita?id="+dati.rows.item(i).codice_visita+"'><button data-theme='f'> Visita del "+dati.rows.item(i).data_inizio_visita+"</button></a>";
                             //datiRiga+='<li><a class="singola_visita_link" href="#singola_visita?id='+dati.rows.item(i).id_locale+'">'+dati.rows.item(i).data_inizio_visita+'</a></li>';
                             //alert("Ancora da visionare: "+dati.rows.item(i).nome);
+                            id_sede=dati.rows.item(i).codice_ispezione.id_sede;
+                            datiRiga+="<li data-role='list-divider' data-theme='a'>"+sedi[id_sede]+"</li>";
                             datiRiga+="<a href='#postazione_mancante?id="+dati.rows.item(i).codice_ispezione+"'><button data-theme='f'> "+dati.rows.item(i).nome+"</button></a>";
                         }
                         $("#listapostazionimancanti").html('');
                         $("#listapostazionimancanti").append(datiRiga);
+                        $('#listapostazionimancanti').listview('refresh');
                         //$('#listapostazionimancanti').page();
                         //$("#postazione_mancante").trigger("create");
                     } else {
@@ -1429,9 +1432,13 @@ function onDeviceReady() {
                                                         //datiRiga+="<a href='#singola_visita?id="+dati.rows.item(i).codice_visita+"'><button data-theme='f'> Visita del "+dati.rows.item(i).data_inizio_visita+"</button></a>";
                                                         //datiRiga+='<li><a class="singola_visita_link" href="#singola_visita?id='+dati.rows.item(i).id_locale+'">'+dati.rows.item(i).data_inizio_visita+'</a></li>';
                                                         //alert("Ancora da visionare: "+dati.rows.item(i).nome);
+                                                        id_sede=dati.rows.item(i).codice_ispezione.id_sede;
+                                                        datiRiga+="<li data-role='list-divider' data-theme='a'>"+sedi[id_sede]+"</li>";
                                                         datiRiga+="<a href='#postazione_mancante?id="+dati.rows.item(i).codice_ispezione+"'><button data-theme='f'> "+dati.rows.item(i).nome+"</button></a>";
                                                     }
+                                                    $("#listapostazionimancanti").html('');
                                                     $("#listapostazionimancanti").append(datiRiga);
+                                                    $("#listapostazionimancanti").listview('refresh');
                                                     //$("#postazione_mancante").trigger("create");
                                                 } else {
                                                     alert("OK! Tutte le postazioni sono state visitate! Compila il modulo del certificato!");
